a2473e0a844b652c2a711cac947d0f4307997a23
RyanServera <ryans987531@hotmail.com>
2022-05-01 20:48:26 -0400

    Clickable Grid Items

--- src/components/UTurnCard.tsx
+++ src/components/UTurnCard.tsx
@@ -1,9 +1,12 @@
 import { Box, Container, Grid } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FactModel } from "../services/FactService";
 
 interface UTurnCardItem {
     factItem: FactModel
+    rowIndex: number
+    columnIndex: number
+    updateGridItemSelected: (rowIndex: number, columnIndex: number) => void
 }
 
 interface UTurnCardProps {
@@ -11,11 +14,18 @@ interface UTurnCardProps {
 }
 
 interface UTurnCardRow {
-    factsRow: FactModel[]
+    factsRowData: FactModel[],
+    rowIndex: number
+    updateGridItemSelected: (rowIndex: number, columnIndex: number) => void
 }
 
 function GridItem(props: UTurnCardItem) {
     const [cardColour, setCardColour] = useState("primary.light")
+
+    function handleCorrectAnswer() {
+        setCardColour("primary.dark")
+        props.updateGridItemSelected(props.rowIndex, props.columnIndex)
+    }
 
     return (
         <Box sx={{
@@ -23,16 +33,21 @@ function GridItem(props: UTurnCardItem) {
             textAlign: 'center',
             height: 100,
             overflowWrap: 'break-word'
-          }} onClick={() => setCardColour("primary.dark")}>{props.factItem.fact}</Box>
+          }} 
+          onClick={handleCorrectAnswer}>{props.factItem.fact}</Box>
     )
 }
 
 function FactRow(props: UTurnCardRow) {
     return (
         <Grid container item spacing={1}>
-            {props.factsRow.map((fact, index) => (
+            {props.factsRowData.map((fact, index) => (
                 <Grid key={`Fact${index}`} item xs={2.4}>
-                    <GridItem factItem={fact}/>
+                    <GridItem factItem={fact} 
+                        updateGridItemSelected={props.updateGridItemSelected}
+                        rowIndex={props.rowIndex}
+                        columnIndex={index}
+                    />
                 </Grid>
             ))}
         </Grid>
@@ -40,11 +55,51 @@ function FactRow(props: UTurnCardRow) {
 }
 
 export const UTurnCard = (props: UTurnCardProps) => {
+    const emptyBooleanMatrix: boolean[][] = []
+    const [isGridItemSelected, setIsGridItemSelected] = useState(emptyBooleanMatrix)
+
+    function updateGridItemSelected(rowIndex: number, columnIndex: number) {
+        if (isGridItemSelected) {
+            const copyOfGridItemSelected: boolean[][] = cloneGrid(isGridItemSelected)
+            copyOfGridItemSelected[rowIndex][columnIndex] = true
+            setIsGridItemSelected(copyOfGridItemSelected)
+        }
+        
+    }
+
+    function cloneGrid(factGrid: boolean[][]) {
+        return factGrid.map((factRow) => [...factRow])
+    }
+
+    function initializeIsGridItemSelected(facts: FactModel[][]) {
+        if (props.facts.length !== 0) {
+            const numberOfRows = facts.length
+            const numberOfColumns = facts[0].length
+            let initialIsGridItemSelected: boolean[][] = []
+            for (let i = 0; i < numberOfRows; i++) {
+                let rowValues = []
+                for (let j = 0; j < numberOfColumns; j++) {
+                    rowValues.push(false)
+                }
+                initialIsGridItemSelected.push(rowValues)
+            }
+            setIsGridItemSelected(initialIsGridItemSelected)
+        }
+    }
+
+    useEffect(() => {
+        initializeIsGridItemSelected(props.facts)
+    }, [props.facts])
+    
     return (
         <Container>
             <Grid container spacing={1}>
-                {props.facts.map((factsRow, index) => (
-                    <FactRow factsRow={factsRow} key={`Row${index}`} />
+                {props.facts.map((factsRowData, index) => (
+                    <FactRow key={`Row${index}`} 
+                        rowIndex={index}
+                        factsRowData={factsRowData}
+                        updateGridItemSelected={updateGridItemSelected}
+                    />
                 ))}
             </Grid>
         </Container>
