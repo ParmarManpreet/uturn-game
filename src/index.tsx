6894a190151d7cc33b4496dd06326c185e35e9c5
RyanServera <ryans987531@hotmail.com>
2022-05-28 21:35:27 -0400

    Fixing spacing

--- src/index.tsx
+++ src/index.tsx
@@ -10,15 +10,26 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 import LangState from './context/lang';
 
+// Dev
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB7DLyA410nmu5iwSKAaaYffa_qhAuw_Yo",
+//   authDomain: "u-turn-development.firebaseapp.com",
+//   databaseURL: "https://u-turn-development-default-rtdb.firebaseio.com",
+//   projectId: "u-turn-development",
+//   storageBucket: "u-turn-development.appspot.com",
+//   messagingSenderId: "1047610039658",
+//   appId: "1:1047610039658:web:f7a9c58422ec8150e01913",
+//   measurementId: "G-Z4D3Z5D2XL"
+// };
+
+// Prod
 const firebaseConfig = {
-  apiKey: "AIzaSyB7DLyA410nmu5iwSKAaaYffa_qhAuw_Yo",
-  authDomain: "u-turn-development.firebaseapp.com",
-  databaseURL: "https://u-turn-development-default-rtdb.firebaseio.com",
-  projectId: "u-turn-development",
-  storageBucket: "u-turn-development.appspot.com",
-  messagingSenderId: "1047610039658",
-  appId: "1:1047610039658:web:f7a9c58422ec8150e01913",
-  measurementId: "G-Z4D3Z5D2XL"
+  apiKey: "AIzaSyD_8CZoxqI5V8q-71CY5BgYz_uNzDvX3qA",
+  authDomain: "uturn-78fe1.firebaseapp.com",
+  projectId: "uturn-78fe1",
+  storageBucket: "uturn-78fe1.appspot.com",
+  messagingSenderId: "336554198721",
+  appId: "1:336554198721:web:98a2729e3584e065207640"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
