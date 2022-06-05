b5a4e34f55559cf6d45e1b90c87d05096c6ae0dd
ParmarManpreet <manpreet.parmar29@hotmail.com>
2022-06-05 17:37:49 -0400

    settings change teamB and client logo

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,8 +1,12 @@
-import { Box, Switch } from "@mui/material";
+import { Box, Button, IconButton, Input, styled, Switch } from "@mui/material";
 import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import FolderIcon from '@mui/icons-material/Folder';
 import { getVisibleScoreState, updateVisibleScoreState } from "../services/GameStatesService";
+import { storage } from "..";
+import { ref } from "firebase/storage";
+import SettingService from "../services/SettingService";
 
 interface SettingsProp {
     translate : (key: string) => string
@@ -10,11 +14,79 @@ interface SettingsProp {
 
 export const SettingsPage = (props : SettingsProp) => {
     const [isScoreVisible, setIsScoreVisible] = useState(false);
-
+    const [companyLogoURL, setCompanyLogoURL] = useState("")
+    const [teamBuildLogoURL, setTeamBuildLogoURL] = useState("")
+    const [hasRetreivedLogo, setHasRetreivedLogo] = useState(false)
+    const types = ['image/png', 'image/jpeg'];
     const handleChangeVisibleScoreSwitch = (event: React.ChangeEvent<HTMLInputElement>) => {
-        updateVisibleScoreState(event.target.checked)
-        setIsScoreVisible(event.target.checked);
+    updateVisibleScoreState(event.target.checked)
+    setIsScoreVisible(event.target.checked);
     };
+    const Input = styled('input')({
+        display: 'none',
+    });
+
+    const handleTeamBuildingLogoUpdate = async (e: {target: { files: any; }; }) => {
+        const file = e.target.files[0]
+
+        if (file && types.includes(file.type)) {
+            await uploadTeamBuildingLogo(file)
+        } else {
+            return;
+        }
+    }
+
+    const handleCompanyLogoUpdate = async (e: {target: { files: any; }; }) => {
+        const file = e.target.files[0]
+
+        if (file && types.includes(file.type)) {
+            await uploadCompanyLogo(file)
+        } else {
+            return;
+        }
+    }
+
+    async function uploadTeamBuildingLogo(file : any) {
+        const storageRef = ref(storage, `teamBuilding-logos/${file}`)
+        const fileURL = URL.createObjectURL(file)
+        if (fileURL) {
+            SettingService.setTeamBuildingLogo(fileURL)
+            setTeamBuildLogoURL(fileURL)
+        }
+    }
+
+    async function uploadCompanyLogo(file : any) {
+        const storageRef = ref(storage, `company-logos/${file}`)
+        const fileURL = URL.createObjectURL(file)
+        if (fileURL) {
+            SettingService.setCompanyLogo(fileURL)
+            setCompanyLogoURL(fileURL)
+        }
+    }
+
+async function initializeTeamBuildLogo() {
+    try {
+        const url = await SettingService.getTeamBuildingLogo()
+        if(url) {
+            setTeamBuildLogoURL(url)
+        }
+        setHasRetreivedLogo(true)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function initializeCompanyLogo() {
+    try {
+        const url = await SettingService.getCompanyLogo()
+        if(url) {
+            setCompanyLogoURL(url)
+        }
+        setHasRetreivedLogo(true)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
     useEffect(() => {
         async function initializeIsScoreVisible() {
@@ -23,7 +95,8 @@ export const SettingsPage = (props : SettingsProp) => {
                 setIsScoreVisible(fetchedIsScoreVisible)
             }
         }
-
+        initializeTeamBuildLogo()
+        initializeCompanyLogo()
         initializeIsScoreVisible()
     }, [])
 
@@ -34,14 +107,62 @@ export const SettingsPage = (props : SettingsProp) => {
                 <h1>{props.translate('settings-title')}</h1>
                 <Box 
                     sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
                         alignItems: 'center',
-                    }}
-                >
-                <span>{props.translate('settings-score')}</span>
-                <Switch aria-label= 'Switch for Score Visibility'
-                    onChange={(e) => handleChangeVisibleScoreSwitch(e)}
-                    checked={isScoreVisible}
-                />
+                        margingBottom: '200px',
+                        }}
+                >                    <span>{props.translate('settings-score')}</span>
+                    <Switch aria-label= 'Switch for Score Visibility'
+                        onChange={(e) => handleChangeVisibleScoreSwitch(e)}
+                        checked={isScoreVisible}
+                    />
+                </Box>
+
+                <Box 
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        margingBottom: '200px',
+                        }}
+                > 
+                    {hasRetreivedLogo? <img src={teamBuildLogoURL} height="32" alt="Logo"/>: null}
+                    <label>
+                    <Input accept="image/*" id="icon-button-file" type="file" onChange={handleTeamBuildingLogoUpdate} />
+                    <IconButton 
+                        sx={{
+                            marginTop: '8px',
+                            color: 'white',
+                        }}
+                        aria-label="upload picture" 
+                        component="span">
+                        <FolderIcon />
+                    </IconButton>
+                </label>
+                </Box>
+
+                <Box 
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        margingBottom: '200px',
+                        }}
+                > 
+                    {hasRetreivedLogo? <img src={companyLogoURL} height="32" alt="Logo"/>: null}
+                    <label>
+                    <Input accept="image/*" id="icon-button-file" type="file" onChange={handleCompanyLogoUpdate} />
+                    <IconButton 
+                        sx={{
+                            marginTop: '8px',
+                            color: 'white',
+                        }}
+                        aria-label="upload picture" 
+                        component="span">
+                        <FolderIcon />
+                    </IconButton>
+                </label>
                 </Box>
             </section>
             <Footer cardProgress={null}
